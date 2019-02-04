5f7c2b6c80e612efaee551f3eb53a528d0340daf
Giampaolo Cimino <gcimino@gmail.com>
2019-02-04 16:58:28 +0100

    delete old bat file

--- conda-official.bat
+++
@@ -1,3 +0,0 @@
-python conda_repo.py -l log_conf.yml -u https://repo.continuum.io/pkgs/main/ win-64 I:\conda-official\main\
-python conda_repo.py -l log_conf.yml -u https://repo.continuum.io/pkgs/free/ win-64 I:\conda-official\free\
-python conda_repo.py -l log_conf.yml -u https://repo.continuum.io/pkgs/pro/ win-64 I:\conda-official\pro\
